2c4ba7e244180592b32ae490a9546dd035c9083a
Zbigniew Romanowski <romz@wp.pl>
2018-05-14 17:49:29 +0200

    Remove some test from polutest

--- tests-suite/cachetest.sh
+++ tests-suite/cachetest.sh
@@ -4,7 +4,7 @@ echo -n "cachetest... "
 cachetest=./tests/cachetest/cachetest
 log=cachetest.log
 rm -rf ${log}
-${cachetest} order -etc -bnum 5000 10000                                     > ${log} || exit 1
+${cachetest} order -etc -bnum 5000 10000                                    >> ${log} || exit 1
 ${cachetest} order -th 4 -bnum 5000 10000                                   >> ${log} || exit 1
 ${cachetest} order -th 4 -rnd -etc -bnum 5000 -capcnt 10000 10000           >> ${log} || exit 1
 ${cachetest} order -th 4 -rnd -etc -bnum 5000 -capsiz 10000 10000           >> ${log} || exit 1
--- tests-suite/polytest.sh
+++ tests-suite/polytest.sh
@@ -4,7 +4,7 @@ echo -n "polytest... "
 polytest=./tests/polytest/polytest
 polymgr=./tests/polymgr/polymgr
 log=polytest.log
-rm -rf casket*
+rm -rf casket*                                                                                          >> ${log} || exit 1
 ${polytest} order -set "casket.kct#bnum=5000#msiz=50000" 10000                                          >> ${log} || exit 1
 ${polytest} order -get "casket.kct#msiz=50000" 10000                                                    >> ${log} || exit 1
 ${polytest} order -getw "casket.kct#msiz=5000" 10000                                                    >> ${log} || exit 1
@@ -49,37 +49,37 @@ ${polytest} index -rem "casket.kct" 10000                                       
 ${polytest} index -etc "casket.kct#idxclim=32k" 10000                                                   >> ${log} || exit 1
 ${polytest} index -th 4 -rnd -set "casket.kct#idxclim=32k#idxdbnum=4" 10000                             >> ${log} || exit 1
 ${polytest} index -th 4 -rnd -get "casket.kct" 10000                                                    >> ${log} || exit 1
-${polytest} index -th 4 -rnd -rem "casket.kct" 10000   >> ${log} || exit 1
-${polytest} index -th 4 -rnd -etc "casket.kct#idxclim=32k#idxdbnum=4" 10000   >> ${log} || exit 1
-rm -rf casket*   >> ${log} || exit 1
-${polytest} order -rnd "casket.kcx" 10000   >> ${log} || exit 1
-${polytest} order -th 4 -rnd "casket.kcx" 10000   >> ${log} || exit 1
-${polytest} wicked "casket.kcx" 10000   >> ${log} || exit 1
-${polytest} wicked -th 4 "casket.kcx" 10000   >> ${log} || exit 1
-${polymgr} list -pv "casket.kcx" > check.out   >> ${log} || exit 1
-${polymgr} list -max 1000 -pv "casket.kcx" > check.out   >> ${log} || exit 1
-${polytest} mapred "casket.kcx" 10000   >> ${log} || exit 1
-${polytest} mapred -xpm -xpr -xpf "casket.kcx" 10000   >> ${log} || exit 1
-rm -rf casket*   >> ${log} || exit 1
-${polytest} order -rnd "casket.kch#opts=s#bnum=256" 1000   >> ${log} || exit 1
-${polytest} order -rnd "casket.kct#opts=l#psiz=256" 1000   >> ${log} || exit 1
-${polytest} order -rnd "casket.kcd#opts=c#bnum=256" 500   >> ${log} || exit 1
-${polytest} order -rnd "casket.kcf#opts=c#psiz=256" 500   >> ${log} || exit 1
-${polytest} order -rnd "casket.kcx" 500   >> ${log} || exit 1
-${polymgr} merge -add "casket#type=kct" casket.kch casket.kct casket.kcd casket.kcf casket.kcx   >> ${log} || exit 1
-rm -rf casket*   >> ${log} || exit 1
-${polytest} misc "casket#type=-"   >> ${log} || exit 1
-${polytest} misc "casket#type=+"   >> ${log} || exit 1
-${polytest} misc "casket#type=:"   >> ${log} || exit 1
-${polytest} misc "casket#type=*#zcomp=def"   >> ${log} || exit 1
-${polytest} misc "casket#type=%#zcomp=gz"   >> ${log} || exit 1
-rm -rf casket*   >> ${log} || exit 1
-${polytest} misc "casket#type=kch#log=-#logkinds=debug#mtrg=-#zcomp=lzocrc"   >> ${log} || exit 1
-rm -rf casket*   >> ${log} || exit 1
-${polytest} misc "casket#type=kct#log=-#logkinds=debug#mtrg=-#zcomp=lzmacrc"   >> ${log} || exit 1
-rm -rf casket*   >> ${log} || exit 1
-${polytest} misc "casket#type=kcd#zcomp=arc#zkey=mikio"   >> ${log} || exit 1
-rm -rf casket*   >> ${log} || exit 1
-${polytest} misc "casket#type=kcf#zcomp=arc#zkey=mikio"   >> ${log} || exit 1
+${polytest} index -th 4 -rnd -rem "casket.kct" 10000                                                    >> ${log} || exit 1
+${polytest} index -th 4 -rnd -etc "casket.kct#idxclim=32k#idxdbnum=4" 10000                             >> ${log} || exit 1
+rm -rf casket*                                                                                          >> ${log} || exit 1
+${polytest} order -rnd "casket.kcx" 10000                                                               >> ${log} || exit 1
+${polytest} order -th 4 -rnd "casket.kcx" 10000                                                         >> ${log} || exit 1
+${polytest} wicked "casket.kcx" 10000                                                                   >> ${log} || exit 1
+${polytest} wicked -th 4 "casket.kcx" 10000                                                             >> ${log} || exit 1
+${polymgr} list -pv "casket.kcx" > check.out                                                            >> ${log} || exit 1
+${polymgr} list -max 1000 -pv "casket.kcx" > check.out                                                  >> ${log} || exit 1
+${polytest} mapred "casket.kcx" 10000                                                                   >> ${log} || exit 1
+${polytest} mapred -xpm -xpr -xpf "casket.kcx" 10000                                                    >> ${log} || exit 1
+rm -rf casket*                                                                                          >> ${log} || exit 1
+${polytest} order -rnd "casket.kch#opts=s#bnum=256" 1000                                                >> ${log} || exit 1
+${polytest} order -rnd "casket.kct#opts=l#psiz=256" 1000                                                >> ${log} || exit 1
+${polytest} order -rnd "casket.kcd#opts=c#bnum=256" 500                                                 >> ${log} || exit 1
+${polytest} order -rnd "casket.kcf#opts=c#psiz=256" 500                                                 >> ${log} || exit 1
+${polytest} order -rnd "casket.kcx" 500                                                                 >> ${log} || exit 1
+${polymgr} merge -add "casket#type=kct" casket.kch casket.kct casket.kcd casket.kcf casket.kcx          >> ${log} || exit 1
+rm -rf casket*                                                                                          >> ${log} || exit 1
+${polytest} misc "casket#type=-"                                                                        >> ${log} || exit 1
+${polytest} misc "casket#type=+"                                                                        >> ${log} || exit 1
+${polytest} misc "casket#type=:"                                                                        >> ${log} || exit 1
+${polytest} misc "casket#type=*#zcomp=def"                                                              >> ${log} || exit 1
+${polytest} misc "casket#type=%#zcomp=gz"                                                               >> ${log} || exit 1
+#rm -rf casket*                                                                                          >> ${log} || exit 1
+#${polytest} misc "casket#type=kch#log=-#logkinds=debug#mtrg=-#zcomp=lzocrc"                             >> ${log} || exit 1
+#rm -rf casket*                                                                                          >> ${log} || exit 1
+#${polytest} misc "casket#type=kct#log=-#logkinds=debug#mtrg=-#zcomp=lzmacrc"                            >> ${log} || exit 1
+#rm -rf casket*                                                                                          >> ${log} || exit 1
+#${polytest} misc "casket#type=kcd#zcomp=arc#zkey=mikio"                                                 >> ${log} || exit 1
+#rm -rf casket*                                                                                          >> ${log} || exit 1
+#${polytest} misc "casket#type=kcf#zcomp=arc#zkey=mikio"                                                 >> ${log} || exit 1
 echo "OK"
 
