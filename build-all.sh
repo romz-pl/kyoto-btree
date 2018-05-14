a0576cedcc786d4b396780e1e973cd0059a19885
Zbigniew Romanowski <romz@wp.pl>
2018-05-14 18:31:03 +0200

    Activate more tests

--- build-all.sh
+++ build-all.sh
@@ -25,16 +25,16 @@ make -j5 || exit 1
 # ctest --verbose
 #ctest
 
-#../tests-suite/cachetest.sh  || exit 1
-#../tests-suite/dirmgr.sh     || exit 1
-#../tests-suite/dirtest.sh    || exit 1
-#../tests-suite/forestmgr.sh  || exit 1
-#../tests-suite/foresttest.sh || exit 1
-#../tests-suite/grasstest.sh  || exit 1
-#../tests-suite/hashmgr.sh    || exit 1
-#../tests-suite/hashtest.sh   || exit 1
-#../tests-suite/langctest.sh  || exit 1
-#../tests-suite/polymgr.sh    || exit 1
+../tests-suite/cachetest.sh  || exit 1
+../tests-suite/dirmgr.sh     || exit 1
+../tests-suite/dirtest.sh    || exit 1
+../tests-suite/forestmgr.sh  || exit 1
+../tests-suite/foresttest.sh || exit 1
+../tests-suite/grasstest.sh  || exit 1
+../tests-suite/hashmgr.sh    || exit 1
+../tests-suite/hashtest.sh   || exit 1
+../tests-suite/langctest.sh  || exit 1
+../tests-suite/polymgr.sh    || exit 1
 ../tests-suite/polytest.sh   || exit 1
 #../tests-suite/prototest.sh  || exit 1
 ../tests-suite/stashtest.sh  || exit 1
