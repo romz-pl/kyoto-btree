68832c6a7a6c792d5ee61c02e4b6585980ddd1a2
Zbigniew Romanowski <romz@wp.pl>
2018-05-14 17:50:02 +0200

    Remove some test from polutest

--- build-all.sh
+++ build-all.sh
@@ -25,23 +25,23 @@ make -j5 || exit 1
 # ctest --verbose
 #ctest
 
-../tests-suite/cachetest.sh  || exit 1
-../tests-suite/dirmgr.sh     || exit 1
-../tests-suite/dirtest.sh    || exit 1
-../tests-suite/forestmgr.sh  || exit 1
-../tests-suite/foresttest.sh || exit 1
-../tests-suite/grasstest.sh  || exit 1
-../tests-suite/hashmgr.sh    || exit 1
-../tests-suite/hashtest.sh   || exit 1
-../tests-suite/langctest.sh  || exit 1
-../tests-suite/polymgr.sh    || exit 1
+#../tests-suite/cachetest.sh  || exit 1
+#../tests-suite/dirmgr.sh     || exit 1
+#../tests-suite/dirtest.sh    || exit 1
+#../tests-suite/forestmgr.sh  || exit 1
+#../tests-suite/foresttest.sh || exit 1
+#../tests-suite/grasstest.sh  || exit 1
+#../tests-suite/hashmgr.sh    || exit 1
+#../tests-suite/hashtest.sh   || exit 1
+#../tests-suite/langctest.sh  || exit 1
+#../tests-suite/polymgr.sh    || exit 1
 ../tests-suite/polytest.sh   || exit 1
-../tests-suite/prototest.sh  || exit 1
-../tests-suite/stashtest.sh  || exit 1
-../tests-suite/treemgr.sh    || exit 1
-../tests-suite/treetest.sh   || exit 1
-../tests-suite/utilmgr.sh    || exit 1
-../tests-suite/utiltest.sh   || exit 1
+#../tests-suite/prototest.sh  || exit 1
+#../tests-suite/stashtest.sh  || exit 1
+#../tests-suite/treemgr.sh    || exit 1
+#../tests-suite/treetest.sh   || exit 1
+#../tests-suite/utilmgr.sh    || exit 1
+#../tests-suite/utiltest.sh   || exit 1
 
 
 
